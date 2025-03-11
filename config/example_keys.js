353654d777147fef6623e0202f4c7df83c562198
Jeremy Dixon <j.dixon.development@gmail.com>
2025-03-11 11:34:15 -0500

    add example keys file

---
+++ config/example_keys.js
@@ -0,0 +1,11 @@
+//Example Keys File
+module.exports = {
+  google: {
+    clientID: 'YOUR_GOOGLE_CLIENT_ID',
+    clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET'
+  },
+  github: {
+    clientID: 'YOUR_GITHUB_CLIENT_ID',
+    clientSecret: 'YOUR_GITHUB_CLIENT_SECRET'
+  }
+};
